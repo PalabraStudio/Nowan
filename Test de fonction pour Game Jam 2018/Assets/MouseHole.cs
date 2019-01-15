89b704a7b7eb6dfb75fd947c6068c46e74324681
BRousserie <32737696+BRousserie@users.noreply.github.com>
2019-01-15 17:31:16 +0100

    Fix affichage souris dans le trou

--- Test de fonction pour Game Jam 2018/Assets/MouseHole.cs
+++ Test de fonction pour Game Jam 2018/Assets/MouseHole.cs
@@ -1,13 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class MouseHole : MonoBehaviour {
+public class MouseHole : MonoBehaviour
+{
     public GameObject personnage;
     public Collider2D collision;
-	
-	// Update is called once per frame
-	void Update () {
-        collision.enabled = !personnage.GetComponent<SpriteRenderer>().sprite.name.StartsWith("mouse");
-	}
+    private TilemapRenderer m_renderer;
+
+    private void Start()
+    {
+        m_renderer = gameObject.GetComponent<TilemapRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (personnage.GetComponent<SpriteRenderer>().sprite.name.StartsWith("mouse"))
+        {
+            collision.enabled = false;
+            gameObject.GetComponent<TilemapRenderer>().sortingLayerName = "Tunnel";
+        }
+        else
+        {
+            collision.enabled = true;
+            gameObject.GetComponent<TilemapRenderer>().sortingLayerName = "Wall";
+        }
+    }
 }
