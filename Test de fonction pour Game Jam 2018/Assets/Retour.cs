547759a65cb812cfcb5cb7ffc31fe5c8d014fe7c
Leon <46721103+LeonPyramid@users.noreply.github.com>
2019-01-19 22:11:26 +0100

    Début Invisibiité/Rangement
    J'ai rajouté un script d'invisibilité, et un petit test de retour visuel pour avoir les infos du temps de recharge.
    J'ai déplacé quelques scripts dans un dossier dedié aux scriptes du personnage

---
+++ Test de fonction pour Game Jam 2018/Assets/Retour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityScript;
+using Unity;
+using Image = UnityEngine.UI.Image;
+
+public class Retour : MonoBehaviour
+{
+    public bool invisibleTest;
+    public bool ready;
+    public Color normal;
+    public Color invisible;
+    public Color charge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        invisibleTest = GameObject.Find("Personnage").GetComponent<Invisibilite>().invisble;
+        ready = GameObject.Find("Personnage").GetComponent<Invisibilite>().ready;
+        if (invisibleTest)
+        {
+            GetComponent<Image>().color = invisible;
+        }
+        else if (!ready)
+        {
+            GetComponent<Image>().color = charge;
+        }
+        else { GetComponent<Image>().color = normal; }
+    }
+}
---
+++ Test de fonction pour Game Jam 2018/Assets/ScriptPerso/Invisibilite.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invisibilite : MonoBehaviour
+{
+    public float invisibleTimer;
+    public float timeCounter;
+    public bool invisble;
+    public bool ready;
+    public float reloadTime;
+    public bool i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCounter = invisibleTimer;
+        invisble = false;
+        ready = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i=Input.GetKeyDown("i");
+        if (i && ready)
+        {
+            invisble = true;
+            ready = false;
+        }
+        if (!ready)
+        {
+            timeCounter -= Time.deltaTime;
+        }
+        if (timeCounter < 0)
+        {
+            invisble = false;
+        }
+        if (timeCounter < -reloadTime)
+        {
+            ready = true;
+            timeCounter = invisibleTimer;
+        }
+
+    }
+}
