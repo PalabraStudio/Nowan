a7d60ec4d59e4563b8525184b12ed48d4dbefb53
BRousserie <32737696+BRousserie@users.noreply.github.com>
2019-01-22 20:01:37 +0100

    menu fonctionnel

---
+++ Nowan/Assets/ScriptButton/HoverButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverButton : MonoBehaviour
+{
+    public Text m_text;
+    private void OnMouseEnter()
+    {
+        m_text.color = new Color(0.9f, 0.9f, 0.9f);
+    }
+    
+    private void OnMouseExit()
+    {
+        m_text.color = new Color(0.2f, 0.2f, 0.2f);
+    }
+}
---
+++ Nowan/Assets/ScriptButton/QuitButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public void OnMouseUp()
+    {
+        Application.Quit();
+
+        //Application.Quit ne fonctionne que sur une version Build (un .exe) du jeu. Pour tester dans Unity Editor, il faut :
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
