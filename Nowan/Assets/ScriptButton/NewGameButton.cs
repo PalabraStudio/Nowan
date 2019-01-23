0bfd72cc8d36197e1cbc8f77fc40b286235f52d2
BRousserie <baptiste.rousserie@etu.u-bordeaux.fr>
2019-01-23 12:09:09 +0100

    Utilisation fonction plus récente LoadScene

--- Nowan/Assets/ScriptButton/NewGameButton.cs
+++ Nowan/Assets/ScriptButton/NewGameButton.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewGameButton : MonoBehaviour
 {
     public void OnMouseUp()
     {
-        Application.LoadLevel("NiveauTest");
+        SceneManager.LoadScene("NiveauTest");
     }
 }
