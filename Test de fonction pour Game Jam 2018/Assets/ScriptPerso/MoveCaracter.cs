e85797321da3f0937a16260f09dfecdab4baebbf
picachoc <loic.lachiver@gmail.com>
2019-01-24 01:15:25 +0100

    Animation

--- Test de fonction pour Game Jam 2018/Assets/ScriptPerso/MoveCaracter.cs
+++ Test de fonction pour Game Jam 2018/Assets/ScriptPerso/MoveCaracter.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public enum STATE { BAT, MOUSE, NORMAL};
 
 public class MoveCaracter : MonoBehaviour {
+
     public float speed;
     public float sprint;
+
     public Rigidbody2D rb2d;
-    private bool m_shift;
+    public Animator animator;
+
+    private bool m_shift; //Touche shift appuyée ou pas
     public bool shift { get { return m_shift; } }
     private float m_horizontal;
     public float horizontal { get { return m_horizontal; } }
     private float m_vertical;
     public float vertical { get { return m_vertical; } }
 
-    void Start () {
-        rb2d = this.GetComponent<Rigidbody2D>();
-	}
+    private bool p_up, p_down, p_left, p_right; //en travaux
+    private bool up, down, left, right;
 
     //Les FixedUpdates ne dépendent pas du framerate du pc
     //ce qui permet de rester propre niveau physique peu
@@ -27,6 +30,14 @@ public class MoveCaracter : MonoBehaviour {
         m_shift = Input.GetKey(KeyCode.LeftShift);
         m_horizontal = Input.GetAxis("Horizontal") ;
         m_vertical = Input.GetAxis("Vertical") ;
+
+        
+        animator.SetBool("up", (m_vertical>0) && (m_horizontal==0));
+        animator.SetBool("down", (m_vertical<0) && (m_horizontal == 0));
+        animator.SetBool("left", m_horizontal<0);
+        animator.SetBool("right", m_horizontal>0);
+
+
 
         if (m_shift)
         {
@@ -40,5 +51,7 @@ public class MoveCaracter : MonoBehaviour {
         }
         Vector2 mouvement = new Vector2 (transform.position.x + m_horizontal,transform.position.y + m_vertical);
         rb2d.MovePosition(mouvement);
+
+        
     }
 }
