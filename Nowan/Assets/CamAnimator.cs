e52ee7eea82399b58389cb938785a12d8d70211e
Leon <46721103+LeonPyramid@users.noreply.github.com>
2019-01-27 02:11:18 +0100

    Camera 1.0
    Ajout de l'objet Camera, avec des animations diverses, montrant quand elle est allumé, un petit sintillement de manière semi-aléatoire, et une animation de detection (sprite fait main, on ne juge pas). Il n'y a qu'un script créé pour le moment, celui qui régit les scintillements.

---
+++ Nowan/Assets/CamAnimator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamAnimator : MonoBehaviour
+{
+    private Animator animator;
+    public Sprite endOfBlink;
+    private bool blinking;
+    private bool alarm;
+    public float rangeBlink;
+    private float time;
+    private bool blinkIsOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time <= 0)
+        {
+            time = rangeBlink+ ((Time.time*10)%rangeBlink);
+            blinking = true;
+        }
+        else if (time != 0 && GetComponent<SpriteRenderer>().sprite == endOfBlink)
+        {
+            blinking = false;
+        }
+        else
+        {
+            time -= Time.deltaTime;
+        }
+            animator.SetBool("blinking", blinking);
+
+    }
+}
