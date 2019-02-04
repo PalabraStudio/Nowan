202ec4140ca49a02d1a2b9d65d934aa6c3306bb0
Leon <46721103+LeonPyramid@users.noreply.github.com>
2019-02-04 12:48:18 +0100

    Début Code GUI et mort

---
+++ Nowan/Assets/RandomChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomChange : MonoBehaviour
+{
+    private int randomNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        randomNum = Random.Range(0, 100);
+        if (randomNum > 90)
+        {
+            this.GetComponent<Animator>().SetBool("Change", !this.GetComponent<Animator>().GetBool("Change"));
+        }
+    }
+}
