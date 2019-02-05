8d744736f57765a4e9f458c367d7c84fd4c37ef4
Leon <46721103+LeonPyramid@users.noreply.github.com>
2019-02-05 20:09:22 +0100

    Pause debut

---
+++ Nowan/Assets/PauseGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public bool paused;
+    public GameObject panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paused = false;
+        panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)&&!paused)
+        {
+            Time.timeScale = 0;
+            panel.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && paused)
+        {
+            Time.timeScale = 1;
+            panel.SetActive(false);
+        }
+        paused = Time.timeScale == 1 ? false : true;
+    }
+}
