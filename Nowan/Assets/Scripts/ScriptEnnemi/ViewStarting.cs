21734b43de6cb5be02939a52dd1cda0d702b7785
Leon <46721103+LeonPyramid@users.noreply.github.com>
2019-02-11 20:39:20 +0100

    Ennemy placées

--- Nowan/Assets/Scripts/ScriptEnnemi/ViewStarting.cs
+++ Nowan/Assets/Scripts/ScriptEnnemi/ViewStarting.cs
@@ -6,9 +6,12 @@ public class ViewStarting : MonoBehaviour
 {
     public enum cote { up,down,left,right}
     public cote choix;
+    public float waiter;
+    public bool test = true;
     // Start is called before the first frame update
     void Start()
     {
+
         if (choix ==cote.up)
         {
             GetComponentInChildren<Raycast>().dup = 1f;
@@ -31,6 +34,34 @@ public class ViewStarting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (waiter > 0)
+        {
+            waiter -= Time.deltaTime;
+        }
+        else if(test)
+        {
+            test = false;
+            if (choix == cote.up)
+            {
+                GetComponentInChildren<Raycast>().dup = 1f;
+                GetComponent<Animator>().SetBool("Up", true);
+            }
+            if (choix == cote.down)
+            {
+                GetComponentInChildren<Raycast>().ddown = 1f;
+                GetComponent<Animator>().SetBool("Down", true);
+            }
+            if (choix == cote.left)
+            {
+                GetComponentInChildren<Raycast>().dleft = 1f;
+                GetComponent<Animator>().SetBool("Left", true);
+            }
+            if (choix == cote.right)
+            {
+                GetComponentInChildren<Raycast>().dright = 1f;
+                GetComponent<Animator>().SetBool("Right", true);
+            }
+            
+        }
     }
 }
